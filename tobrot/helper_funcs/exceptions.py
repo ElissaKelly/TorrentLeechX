4794267c3ff52f816119acb56f9fc8aed10ab125
ElissaKelly <91330469+ElissaKelly@users.noreply.github.com>
2021-11-24 11:34:09 +0800

    Delete tobrot directory

--- tobrot/helper_funcs/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DirectDownloadLinkException(Exception):
-    """No method found to download from that Direct Link :("""
-    pass
-
-
-class NotSupportedExtractionArchive(Exception):
-    """The Archive Format you are trying to Extract Is Not Supported"""
-    pass
